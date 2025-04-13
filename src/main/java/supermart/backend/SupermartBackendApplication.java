67b348ca480d62a522f441c373bbab11f886291f
rtksansare <sansarerutik19@gmail.com>
2025-04-13 10:37:16 +0530

    Initial commit

---
+++ src/main/java/supermart/backend/SupermartBackendApplication.java
@@ -0,0 +1,13 @@
+package supermart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/supermart/backend/SupermartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package supermart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
